220ca13359bb14fa1ab75769768436cd1faf9379
cmlozanos <carlosmlozanos@gmail.com>
2021-01-27 14:16:23 +0100

    fix test - step 3.1 - change report format

--- src/test/java/com/kata/marsrover/MarsRoverTest.java
+++ src/test/java/com/kata/marsrover/MarsRoverTest.java
@@ -23,7 +23,7 @@ public class MarsRoverTest {
 		}
 
 		public String report() {
-			return String.format("%d,%d %s", x, y, direction);
+			return String.format("(%d, %d) %s", x, y, direction);
 		}
 	}
 
@@ -36,6 +36,6 @@ public class MarsRoverTest {
 	void testRoverCreation(int x, int y, String direction) {
     	var rover = new Rover(x,y,direction);
 		assertEquals("Rover", rover.getClass().getSimpleName());
-    	assertEquals(String.format("(%d,%d) %s", x, y, direction), rover.report());
+    	assertEquals(String.format("(%d, %d) %s", x, y, direction), rover.report());
     }
 }
