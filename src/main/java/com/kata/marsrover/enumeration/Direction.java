f7908963af565e05a5e0a62fadb9dc6f98812fe5
cmlozanos <carlosmlozanos@gmail.com>
2021-01-30 19:25:26 +0100

    fix test - steps 1.1, 1.2, 1.3 - next direction

--- src/main/java/com/kata/marsrover/enumeration/Direction.java
+++ src/main/java/com/kata/marsrover/enumeration/Direction.java
@@ -4,6 +4,13 @@ public enum Direction {
 	NORTH, EAST, SOUTH, WEST;
 
 	Direction next() {
-		return Direction.EAST;
+		if(isLastElement()) {
+			return Direction.values()[0];
+		}
+		return Direction.values()[this.ordinal() + 1];
+	}
+
+	private boolean isLastElement() {
+		return this.ordinal() == Direction.values().length - 1;
 	}
 }
