6bced91e1fe85026ed1370b9ecce41f7d813b3d9
cmlozanos <carlosmlozanos@gmail.com>
2021-01-30 14:56:43 +0100

    refactor coordinates creation

---
+++ src/main/java/com/kata/marsrover/Coordinates.java
@@ -0,0 +1,21 @@
+package com.kata.marsrover;
+
+import com.kata.marsrover.enumeration.Direction;
+
+public class Coordinates {
+	private final Point point;
+	private final Direction direction;
+
+	public Coordinates(Point point, Direction direction) {
+		this.point = point;
+		this.direction = direction;
+	}
+
+	public Point getPoint() {
+		return this.point;
+	}
+
+	public Direction getDirection() {
+		return direction;
+	}
+}
